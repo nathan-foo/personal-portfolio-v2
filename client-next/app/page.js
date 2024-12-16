170e280ff440b22464d9f90650cbb8d7d804a3c8
Nathan Foo <ntfoo27@outlook.com>
2024-12-16 16:26:12 -0600

    Organize sections

--- client-next/app/page.js
+++ client-next/app/page.js
@@ -1,7 +1,17 @@
+import About from "@/components/About";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
     <div>
-      Hello world
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      <Footer />
     </div>
   );
 }
